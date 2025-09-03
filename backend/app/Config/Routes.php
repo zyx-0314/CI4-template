8013d277492f77b8874d14bcbc2d55a8f4ef5ffc
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-09-04 04:25:54 +0800

    feat: Update routes to direct home and mood board to User controller

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -5,4 +5,5 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'User::index');
+$routes->get('mood-board', 'User::moodBoard');
