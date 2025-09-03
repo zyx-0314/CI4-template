5925f44aa168cc7b4e0d95a63889920450ee0f3a
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-09-04 04:46:28 +0800

    feat: Add routes for services pages in User controller

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -8,3 +8,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'User::index');
 $routes->get('mood-board', 'User::moodBoard');
 $routes->get('roadmap', 'User::roadmap');
+// Services pages (dynamic): /services and /services/{style}
+$routes->get('services', 'User::services');
+$routes->get('services/(:segment)', 'User::services/$1');
