79604abd699981ca426b722592b49f6d7f61e327
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-09-05 07:56:37 +0800

    feat: add ClearDatabaseSeeder for truncating funeral_requests table

---
+++ backend/app/Database/Seeds/ClearDatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class ClearDatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('funeral_requests');
+
+        // Use disableForeignKeyChecks if supported by the DB to avoid FK issues
+        $db->disableForeignKeyChecks();
+        $builder->truncate();
+        $db->enableForeignKeyChecks();
+    }
+}
